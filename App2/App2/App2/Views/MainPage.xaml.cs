cd584a5e49ef028dbd917453d08f5f39eccc2661
Vacheron thibaut <blacpiece@gmail.com>
2019-02-13 15:46:34 +0100

    +test App2

---
+++ App2/App2/App2/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }  
+
+        public event void click()
+        {
+            message.Text = "tu as click";
+        }
+    }
+}
