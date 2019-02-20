9b6ae38ea00b0ace70326675aa3c32e8e8d41377
Vacheron thibaut <blacpiece@gmail.com>
2019-02-20 10:51:31 +0100

    + essaie LIsteView
    tuto internet

---
+++ App3/App3/App3/Object/appcell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class appcell : ViewCell
+    {
+        Label nameLabel, ageLabel, locationLabel;
+
+        public static readonly BindableProperty NameProperty =
+            BindableProperty.Create("Name", typeof(string), typeof(CustomCell), "Name");
+        public static readonly BindableProperty AgeProperty =
+            BindableProperty.Create("Age", typeof(int), typeof(CustomCell), 0);
+        public static readonly BindableProperty LocationProperty =
+            BindableProperty.Create("Location", typeof(string), typeof(CustomCell), "Location");
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public int Age
+        {
+            get { return (int)GetValue(AgeProperty); }
+            set { SetValue(AgeProperty, value); }
+        }
+
+        public string Location
+        {
+            get { return (string)GetValue(LocationProperty); }
+            set { SetValue(LocationProperty, value); }
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            if (BindingContext != null)
+            {
+                nameLabel.Text = Name;
+                ageLabel.Text = Age.ToString();
+                locationLabel.Text = Location;
+            }
+        }
+    }
+}
